f77cb024340b2a11fd1f415d15665d767f1a4cb4
unknown <phucnvph24795@fpt.edu.vn>
2022-06-15 10:59:44 +0700

    first commit

---
+++ study/day6/bien.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 20;
+
+function sum (a, b){
+    // kiểm tra 
+    if (typeof a !== "number" || typeof a !== "number"); return -1;
+    // xử lý 
+    const result = a + b;
+    // kết quả
+    return result;
+}
+
+console.log(sum(a, b));
---
+++ study/day6/kieudulieu.js
@@ -0,0 +1,13 @@
+const myage = 10; //number
+const myname = "đạt" //string
+const isstatus = true //bolean
+
+
+console.log(myname);
+console.log(running());
+
+const myinfo= {
+    name: "đạt",
+    age: 10,
+    isstatu
+}
